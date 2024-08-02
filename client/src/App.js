bce0414ee3396cc40058264cec7d911382e3a988
Yuvraj Singh <131570406+yuvrajsingh08@users.noreply.github.com>
2024-08-02 22:58:57 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
